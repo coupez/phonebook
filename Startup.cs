690d0b2c42f932636cadfc1a4556db15ae052ad9
Lucas Coupez <lucascoupez@gmail.com>
2020-05-14 12:50:40 -0600

    link contact service

--- Startup.cs
+++ Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using phonebook.Services;
 
 namespace phonebook
 {
@@ -20,6 +21,7 @@ namespace phonebook
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IContactService, ContactService>();
 
             services.AddControllersWithViews();
 
